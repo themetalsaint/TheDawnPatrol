e8c0685df25badf7d027ab2f6e0556243da98565
Mark June <marcusjune95@gmail.com>
2021-05-06 16:20:28 -0400

    added js-code that gets beach-name from user, and display the current weather of the beach

---
+++ assests/js/script.js
@@ -0,0 +1,73 @@
+
+//add NC to userInput
+var userInput = 'Carolina Beach' //'get the input from html form'
+var beachName = userInput + ' NC' //userInput + ' NC'
+
+//Using positionStackApi, fetch data by beach-name, and get latitude, and longitude of the beach 
+function getData(){
+    var psApiKey = '8e190ed679ba752690fd2008444f7cd2'
+    var positionStackApi = `http://api.positionstack.com/v1/forward?access_key=${psApiKey}&query=${beachName}&country=US`
+    fetch(positionStackApi)
+    .then(function(response){
+        response.json()
+        .then(function(beach){
+            var lat = beach.data[0].latitude
+            var long = beach.data[0].longitude
+            getForecastWeather(lat,long)
+        })
+    })   
+
+}
+
+
+//By using OpenWeatherApi, accept lat,long to retrieve current weather information
+function getForecastWeather(lat,long){
+    var forecastKey = 'a2b4c3401daabb98bf05eae4890ac57c'
+    var forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=hourly,minutely,alerts&appid=${forecastKey}`
+    fetch(forecastWeatherUrl)
+        .then(function(response){
+            response.json().then(function(weather){
+            console.log(weather)
+            displayWeather(weather)
+        })
+    })
+}
+
+//display the current weather info to the page
+function displayWeather(weatherData){
+    var weatherCardEl = $('.weather-forecast')
+
+    var beachEl = $('<h3>')
+    beachEl.addClass('text-style beach-name')
+    beachEl.text(userInput)
+
+    var currentEl = $('<h3>')
+    currentEl.addClass('text-style')
+    currentEl.text('Now')
+
+    var iconEl = $('<img>')
+    iconEl.attr('src',`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`)
+    iconEl.addClass('current-img text-style')
+    
+    var tempEl = $('<p>')
+    tempEl.addClass('text-style')
+    tempEl.text(`Temp: ${weatherData.current.temp} \xB0F`)
+
+    var windEl = $('<p>')
+    windEl.addClass('text-style')
+    windEl.text(`Wind Speed: ${weatherData.current.wind_speed} MPH`)
+
+    var humidityEl = $('<p>')
+    humidityEl.addClass('text-style')
+    humidityEl.text(`Humidity: ${weatherData.current.humidity}%`)
+
+    var uvEl = $('<p>')
+    uvEl.addClass('text-style')
+    uvEl.text(`UV Index: (${weatherData.current.uvi})`)
+
+    weatherCardEl.append( beachEl, iconEl, currentEl, tempEl, windEl, humidityEl, uvEl)
+
+}
+
+//invoke function
+getData()
